d2e4398861e0ae31f5b5d30c3679670ea1131de9
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-08-10 20:13:13 +0530

    Create p40.java

---
+++ p40.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,n,a,b,c;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		a=0;
+		b=1;
+		System.out.println(a);
+		System.out.println(b);
+		for(i=3;i<=n;i++)
+		{
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+	}
+}
