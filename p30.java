0f5484c8e451635ab2c265909614d32884f3b8fe
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-08-11 12:51:33 +0530

    Update p30.java

--- p30.java
+++ p30.java
@@ -12,8 +12,8 @@ class Ideone
 		int h1,h2,h3,h,m1,m2,m;
 		Scanner s=new Scanner(System.in);
 		h1=s.nextInt();
-		h2=s.nextInt();
 		m1=s.nextInt();
+		h2=s.nextInt();
 		m2=s.nextInt();
 		h1=h1*60+m1;
 		h2=h2*60+m2;
