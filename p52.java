c037fd39a34363c67a039afe97d816681911b5b8
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-08-14 13:12:15 +0530

    Create p52.java

---
+++ p52.java
@@ -0,0 +1,47 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i;
+		Scanner s=new Scanner(System.in);
+		i=s.nextInt();
+		switch(i)
+		{
+			    case 1:
+				System.out.println("One");
+				break;
+				case 2:
+				System.out.println("Two");
+				break;
+				case 3:
+				System.out.println("Three");
+				break;
+				case 4:
+				System.out.println("Four");
+				break;
+				case 5:
+				System.out.println("Five");
+				break;
+				case 6:
+				System.out.println("Six");
+				break;
+	
+				case 7:
+				System.out.println("Seven");
+				break;
+				case 8:
+				System.out.println("Eight");
+				break;
+				default:
+				System.out.println("enter correct choice");
+		}
+		
+	}
+}
