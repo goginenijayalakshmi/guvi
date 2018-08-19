3f33e818929fd4b5cb2cef2401d58db75920adff
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-08-19 11:24:55 +0530

    Create j66.java

---
+++ j66.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.println("no");
+				break;
+			}
+			else
+			continue;
+		}
+		if(i==n)
+		System.out.println("yes");
+	}
+}
