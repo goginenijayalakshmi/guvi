a6c8383361adb3500dcecfadb124ef02d925d834
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-08-19 10:17:16 +0530

    Create j58.java

---
+++ j58.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int n,k,i;
+	    boolean found=false;
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+	    k=s.nextInt();
+	    int a[]=new int[n];
+	    for(i=0;i<n;i++)
+	    a[i]=s.nextInt();
+	    for(i=0;i<n;i++)
+	    {
+	    	if(a[i]==k)
+	    	found=true;
+	    }
+	    if(found)
+	    System.out.println("yes");
+	    else
+	    System.out.println("no");
+ 
+	}
+}
