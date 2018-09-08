f05b1eda42458b40194c9b1a8b1d249c05ec7bba
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-09-08 14:58:53 +0530

    Create s36.java

---
+++ s36.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,j=0;
+		String s1;
+		Scanner s=new Scanner(System.in);
+		s1=s.next(); 
+		char[] a=s1.toCharArray();
+		char c;
+		c=s.next().charAt(0);
+		for(i=0;i<s1.length();i++)
+		{
+			if(a[i]==c)
+			j++;
+		}
+		System.out.println(j);
+	}
+}
