74c950f49b08ce8d61a640bd1a5145fddf9aad8a
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-09-10 12:53:30 +0530

    Create s57.java

---
+++ s57.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s1;
+		int i,k=0;
+		Scanner s=new Scanner(System.in);
+		s1=s.next();
+		char[] a=s1.toCharArray();
+		char j=s.next().charAt(0);
+		for(i=0;i<s1.length();i++)
+		{
+			if(a[i]==j)
+			{
+		                k++;
+	                 	}
+		}
+		System.out.println(k);
+	}
+}
