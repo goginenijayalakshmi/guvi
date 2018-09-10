820890d9b994af85621214d5a197d8b3d6b93da6
goginenijayalakshmi <41769817+goginenijayalakshmi@users.noreply.github.com>
2018-09-10 12:24:41 +0530

    Create s54.java

---
+++ s54.java
@@ -0,0 +1,37 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s1,s2;
+		int i;
+		boolean flag=false;
+		Scanner s=new Scanner(System.in);
+		s1=s.next();
+		s2=s.next();
+		if(s1.length()==s2.length())
+		{
+		for(i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)==s2.charAt(i))
+			flag=true;
+			else
+			{
+				flag=false;
+				System.out.println("no");
+				break;
+			}
+		}
+		if(flag)
+		System.out.println("yes");
+		}
+		else
+		System.out.println("no");
+	}
+}
